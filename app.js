e6c6690c8de0aa62ad7882370ed1230b0988a9ff
bbeals6 <bbeals6@ivytech.edu>
2023-11-12 13:03:45 -0600

    router

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogRoutes = require('./routes/blogRoutes')
 
 const dbURI = 'mongodb+srv://netjinja:test1234@cluster0.3ypf18c.mongodb.net/nodetuts?retryWrites=true&w=majority'
 const app = express()
@@ -24,42 +24,11 @@ app.get('/', (req, res) => {
   res.redirect('/blogs')
 })
 
-app.get('/blogs/:id', (req, res) => {
-  Blog.findById(req.params.id)
-    .then(rv => res.render('details', { title: 'Blog Details', blog: rv }))
-    .catch(err => console.log(err))
-})
-
-app.delete('/blogs/:id', (req, res) => {
-  Blog.findByIdAndDelete(req.params.id)
-    .then(rv => {
-      res.json({ redirect: '/blogs' })
-    })
-    .catch(err => console.log(err))
-})
-
-app.get('/blogs', (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
-    .then(rv => {
-      res.render('index', { title: 'All Blogs', blogs: rv })
-    })
-    .catch(err => console.log(err))
-})
-
-app.post('/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-  blog.save()
-    .then(() => res.redirect('/'))
-    .catch(err => console.log(err))
-})
-
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' })
 })
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create' })
-})
+app.use(blogRoutes)
 
 app.use((req, res) => {
   res.status(404).render('404', { title: '404 Not Found' })
---
+++ routes/blogRoutes.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const Blog = require('../models/blog')
+
+const router = express.Router()
+
+router.get('/blogs/:id', (req, res) => {
+  Blog.findById(req.params.id)
+    .then(rv => res.render('details', { title: 'Blog Details', blog: rv }))
+    .catch(err => console.log(err))
+})
+
+router.delete('/blogs/:id', (req, res) => {
+  Blog.findByIdAndDelete(req.params.id)
+    .then(rv => {
+      res.json({ redirect: '/blogs' })
+    })
+    .catch(err => console.log(err))
+})
+
+router.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then(rv => {
+      res.render('index', { title: 'All Blogs', blogs: rv })
+    })
+    .catch(err => console.log(err))
+})
+
+router.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body)
+  blog.save()
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
+router.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create' })
+})
+
+module.exports = router
